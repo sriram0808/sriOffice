51b5c6e860b1df13a333631bf2242e5677c4aed7
sriram0808 <sriram08082000@gmail.com>
2024-06-29 14:53:18 +0530

    terms and condition and privacy policy completed

--- src/Components/Privacypolicy.jsx
+++ src/Components/Privacypolicy.jsx
@@ -8,26 +8,26 @@ let Privacypolicy=()=>{
                 <h1 className="text-center privacyTitle my-4">Privacy Policy</h1>
             </div>
             <div className="row">
-                <p className="text-center privacyDiscription">Discription Discription Discription Discription Discription Discription Discription Discription Discription Discription</p>
+                <p className="text-center privacyDiscription my-3">Discription Discription Discription Discription Discription Discription Discription Discription Discription Discription</p>
             </div>
-            <div className="row">
+            <div className="row my-3">
                 <h1 className="text-start privacyQustion">1. Why We Collect your Information?</h1>
-                <p className="tex-start privacyDetails fw-light">We collect user information to fulfill your requests and meet your needs. We are committed to protecting the information you provide to us through the Website. We do not share or sell user information with third parties unless required by law.</p>
-                <h1 className="text-start privacyQustion">2.	What steps does your organization take to protect and manage personally identifiable information (Personal Information) it receives?</h1>
+                <p className="tex-start privacyDetails fw-light">We collect user information to fulfill your requests and meet your needs. We are committed to protecting the information you provide to us through the Website. We do not share or sell user information with third parties unless required by law.</p> 
+                <h1 className="text-start privacyQustion">2. What steps does your organization take to protect and manage personally identifiable information (Personal Information) it receives?</h1>
                 <p className="tex-start privacyDetails fw-light">Yes. we are committed to data security compliance. We recognize that you need to protect and manage any personally identifiable information ("Personal Information") we receive.</p>
-                <h1 className="text-start privacyQustion">3.	How do you use information from internet access logs to understand what parts of your website visitors are looking at?</h1>
+                <h1 className="text-start privacyQustion">3. How do you use information from internet access logs to understand what parts of your website visitors are looking at?</h1>
                 <p className="tex-start privacyDetails fw-light">Through our Internet access logs, we collect indirect information about Internet visitors. This information is used to determine which pages, sections and information visitors can access</p>
-                <h1 className="text-start privacyQustion">4.	How does Thamizhan Solution ensure the security of my personal information when it's transferred to countries with different privacy laws? </h1>
+                <h1 className="text-start privacyQustion">4. How does Thamizhan Solution ensure the security of my personal information when it's transferred to countries with different privacy laws? </h1>
                 <p className="tex-start privacyDetails fw-light">We prioritize compliance with global privacy laws to protect your personal information. While legal requirements differ by country, we adhere to the principles outlined in our Online Privacy Policy. This includes safeguarding your personal data during international transfers, even to countries with varying privacy standards. Our commitment is to ensure the security of your information wherever it is stored, transferred, or processed.</p>
-                <h1 className="text-start privacyQustion">5.	Can you explain how your website uses cookies to collect general information and how I can manage my preferences to disable them?</h1>
+                <h1 className="text-start privacyQustion">5. Can you explain how your website uses cookies to collect general information and how I can manage my preferences to disable them?</h1>
                 <p className="tex-start privacyDetails fw-light">Our website may use cookies to collect general information. Cookies alone cannot identify any user unless they provide specific personal information. You have the option to change your browser settings to disable cookies through the preferences or options menu.</p>
-                <h1 className="text-start privacyQustion">6.	How does Thamizhan Solution ensure that my personal information remains secure within your organization when I provide it via email?</h1>
+                <h1 className="text-start privacyQustion">6. How does Thamizhan Solution ensure that my personal information remains secure within your organization when I provide it via email?</h1>
                 <p className="tex-start privacyDetails fw-light">At Thamizhan Solution, we do not rent, sell, or share your personal information with non-affiliated third parties or companies. If you have provided us with user information via email, we ensure your privacy by restricting access to within the Thamizhan Solution team responsible for responding to your requests, whether directly or indirectly.</p>
-                <h1 className="text-start privacyQustion ">7.	What features or services would I be unable to access or use if I choose not to provide certain personal information to your company?"</h1>
+                <h1 className="text-start privacyQustion ">7. What features or services would I be unable to access or use if I choose not to provide certain personal information to your company?"</h1>
                 <p className="tex-start privacyDetails fw-light">You may choose not to provide us with certain information, but that may result in you being unable to use certain features of our Services.</p>
-                <h1 className="text-start privacyQustion">8.	Why does your organization reserve the right to replace the privatives coverage, and the way will I be knowledgeable of those adjustments?</h1>
+                <h1 className="text-start privacyQustion">8. Why does your organization reserve the right to replace the privatives coverage, and the way will I be knowledgeable of those adjustments?</h1>
                 <p className="tex-start privacyDetails fw-light">We can also replace this privacy policy once in a while that allows you to mirror, as an instance, modifications to our practices or for different operational, prison, or regulatory reasons.</p>
-                <h1 className="text-start privacyQustion">9.	Contact Us:</h1>
+                <h1 className="text-start privacyQustion">9. Contact Us:</h1>
                 <p className="tex-start privacyDetails fw-light">    If you have any questions about this Privacy Policy, please contact us at thamizhansolutions1630@gmail.com.</p>
             </div>
            </div>
