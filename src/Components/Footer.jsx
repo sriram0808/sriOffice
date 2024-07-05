d5ad482990aedeba4caa3e12ee17bbf1b77425fd
keppuraja3 <keppuraja3@gmail.com>
2024-07-05 16:33:12 +0530

    New logo changed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -68,7 +68,7 @@ let Footer = () => {
               <div className="footer-logo-wrapper">
                 <a href="/">
                   <img
-                    src="https://res.cloudinary.com/deowghzbg/image/upload/v1720174080/Logo_with_Text_1_bazphb.png"
+                    src="https://res.cloudinary.com/deowghzbg/image/upload/v1720176833/TSolu_ugibmk.png"
                     alt="Thamizhan Solution Logo"
                     width="179"
                     height="60"
