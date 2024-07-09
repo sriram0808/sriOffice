c4db9f7f898c872cf5b48c5148a72065b68651f6
sriram0808 <sriram08082000@gmail.com>
2024-07-09 17:22:07 +0530

    changed by keppu

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -374,10 +374,6 @@ let Contact = () => {
               title="Thamizhan Solutions Office"
               width="100%"
               height="400"
-              frameBorder="0"
-              scrolling="no"
-              marginHeight="0"
-              marginWidth="0"
               src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=21,%20Pandian%20Street,%20TVS%20nagar%20Madurai+(Thamizhan%20Solutions)&amp;t=m&amp;z=13&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
             ></iframe>
           </div>
