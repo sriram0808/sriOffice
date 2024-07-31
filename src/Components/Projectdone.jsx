32dbab34865dd3914ef87dec9b1e21484bfdcabf
sriram0808 <sriram08082000@gmail.com>
2024-07-31 11:25:48 +0530

    bysri updated project cards

--- src/Components/Projectdone.jsx
+++ src/Components/Projectdone.jsx
@@ -8,34 +8,24 @@ const Projectdone = () => {
     return (
         <div className="projectBlock">
             <div className="container">
-                <div className="row d-flex justify-content-center">
-                    <div className="col-lg-4 col-md-6 col-sm-12 mt-5 mb-5 d-flex justify-content-center">
+                <div className="row d-flex justify-content-center">  
+                    <div className="col-lg-6 col-md-6 col-sm-12 mt-5 mb-5 d-flex justify-content-center">
                         <div className="card projectCard h-100">
-                            <div className="card-body text-center">
-                                <h2 className='projectCount'>
-                                    <CountUp start={0} end={10} duration={2.75} suffix="+" enableScrollSpy = {true} />
-                                </h2>
-                                <h2 className='prjectSubTitle'>Countries</h2>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6 col-sm-12 mt-5 mb-5 d-flex justify-content-center">
-                        <div className="card projectCard h-100">
-                            <div className="card-body text-center">
-                                <h2 className='projectCount'>
-                                    <CountUp start={0} end={20} duration={2.75} suffix="+" enableScrollSpy = {true} />
-                                </h2>
-                                <h2 className='prjectSubTitle'>Project Delivered</h2>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6 col-sm-12 mt-5 mb-5 d-flex justify-content-center">
-                        <div className="card projectCard h-100">
-                            <div className="card-body text-center">
+                            <div className="card-body d-flex flex-column align-items-center justify-content-center ">
                                 <h2 className='projectCount'>
                                     <CountUp start={0} end={20} duration={2.75} suffix="+" enableScrollSpy = {true} />
                                 </h2>
                                 <h2 className='prjectSubTitle'>Total Project</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-6 col-md-6 col-sm-12 mt-5 mb-5 d-flex justify-content-center">
+                        <div className="card projectCard h-100">
+                            <div className="card-body d-flex flex-column align-items-center justify-content-center ">
+                                <h2 className='projectCount'>
+                                    <CountUp start={0} end={15} duration={2.75} suffix="+" enableScrollSpy = {true} />
+                                </h2>
+                                <h2 className='prjectSubTitle'>Project Delivered</h2>
                             </div>
                         </div>
                     </div>
